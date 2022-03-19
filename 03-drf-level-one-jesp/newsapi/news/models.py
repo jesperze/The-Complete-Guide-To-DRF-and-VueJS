a2db0115ae74f6e86ef578f9c2bc4f0017899f99
J Z E <jesperzeriksson@gmail.com>
2022-03-19 13:26:57 +0100

    Fix publication_date type

--- 03-drf-level-one-jesp/newsapi/news/models.py
+++ 03-drf-level-one-jesp/newsapi/news/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     description = models.CharField(max_length=200)
     body = models.TextField()
     location = models.CharField(max_length=120)
+    publication_date = models.DateField()
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
